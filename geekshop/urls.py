922abdfe5c1fdef77267282191e62a7be76b9b68
n.taradanov@gmail.com <n.taradanov@gmail.com>
2022-03-01 09:14:13 +0700

    lesson_1. Working static site Добросил настройки

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+import mainapp.views as mainapp
+
+urlpatterns =[
+    path("admin/", admin.site.urls),
+    path("", mainapp.main),
+    path("products/", mainapp.products),
+    path("contact/", mainapp.contact),
 ]
